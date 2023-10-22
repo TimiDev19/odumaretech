b5fb46d2a91d77972899eaa9dfa85830003f1178
Timi <86475926+TimiDev19@users.noreply.github.com>
2023-10-22 18:17:15 +0100

    Add files via upload

---
+++ Odumare tech/index.js
@@ -0,0 +1,55 @@
+consttyped = new Typed('.multiple-text', {
+    strings: ['Data Analyst', 'Project Manager', 'Scrum Master', 'Ui/Ux Designer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200 
+   });
+
+   ScrollReveal().reveal('.heading', { origin: 'top'});
+   ScrollReveal().reveal('.footer-list, .stack-container, projects-box, .contact-form', { origin: 'bottom'});
+   ScrollReveal().reveal('.course-card, .about-img', { origin: 'left'});
+   ScrollReveal().reveal('.home-content p, .intro-box', { origin: 'right'});
+
+   let menuIcon = document.querySelector('#menu-icon');
+   let navbar = document.querySelector('.navbar');
+   
+   menuIcon.onclick = () => {
+       menuIcon.classList.toggle('bx-x');
+       navbar.classList.toggle('active');
+   };
+   
+   let sections = document.querySelectorAll('section');
+   let navLinks = document.querySelectorAll('header nav a');
+   
+   window.onscroll = () => {
+       sections.forEach(sec => {
+           let top = window.scrollY;
+           let offset = sec.offsetTop - 150;
+           let height = sec.offsetHeight;
+           let id = sec.getAttribute('id');
+   
+           if(top >= offset && top < offset + height) {
+               navLinks.forEach(links => {
+                   links.classList.remove('active');
+                   document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+               });
+           };
+       });
+   
+   
+   
+       let header = document.querySelector('header');
+   
+       header.classList.toggle('sticky', window.scrollY > 100);
+   
+       menuIcon.classList.remove('bx-x');
+       navbar.classList.remove('active');  
+   };
